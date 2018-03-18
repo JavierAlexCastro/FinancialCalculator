4bda962cfe6b22c40bc7d9ec6a6682f1d806157b
JavierAlexCastro <8484+JavierAlexCastro@users.noreply.github.com>
2018-03-18 00:14:43 -0500

    Initial commit

---
+++ app/src/main/java/javiercastro/financialcalculator/CalculatorActivity.java
@@ -0,0 +1,16 @@
+package javiercastro.financialcalculator;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
---
+++ app/src/main/java/javiercastro/financialcalculator/HomeActivity.java
@@ -0,0 +1,38 @@
+package javiercastro.financialcalculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        final Button start_btn = (Button) findViewById(R.id.start_btn);
+        final Button about_btn = (Button) findViewById(R.id.about_btn);
+
+        start_btn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                Intent calculator = new Intent(HomeActivity.this, CalculatorActivity.class);
+                startActivity(calculator);
+
+            }
+        });
+
+        about_btn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                Intent about = new Intent(HomeActivity.this, AboutActivity.class);
+                startActivity(about);
+
+            }
+        });
+
+    }
+}
