1a29d18a8fd4cd7ef82c7312c3a99265b8a989e2
JavierAlexCastro <8484+JavierAlexCastro@users.noreply.github.com>
2018-04-10 19:00:15 -0500

    Added date picker to Input Activity

--- app/src/main/java/javiercastro/financialcalculator/CalculatorActivity.java
+++ app/src/main/java/javiercastro/financialcalculator/CalculatorActivity.java
@@ -1,16 +1,91 @@
 package javiercastro.financialcalculator;
 
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Typeface;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.icu.text.DateFormat;
+import android.icu.util.Calendar;
+import android.icu.util.GregorianCalendar;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-public class CalculatorActivity extends AppCompatActivity {
+public class CalculatorActivity extends AppCompatActivity
+        implements DatePickerDialog.OnDateSetListener  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        Button continue_btn = (Button)findViewById(R.id.button_continue);
+
+        continue_btn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                //launch new activity
+
+            }
+        });
+    }
+
+    /**
+     * This callback method, call DatePickerFragment class,
+     * DatePickerFragment class returns calendar view.
+     * @param view
+     */
+    public void datePicker(View view){
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.show(getSupportFragmentManager(), "date");
+    }
+
+    /**
+     * To set date on TextView
+     * @param calendar
+     */
+    private void setDate(final Calendar calendar) {
+        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+
+        ((TextView) findViewById(R.id.input_date_d))
+                .setText(dateFormat.format(calendar.getTime()));
+
+    }
+
+    /**
+     * To receive a callback when the user sets the date.
+     * @param view
+     * @param year
+     * @param month
+     * @param day
+     */
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        Calendar cal = new GregorianCalendar(year, month, day);
+        setDate(cal);
+    }
+
+    /**
+     * Create a DatePickerFragment class that extends DialogFragment.
+     * Define the onCreateDialog() method to return an instance of DatePickerDialog
+     */
+    public static class DatePickerFragment extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+
+            return new DatePickerDialog(getActivity(),
+                    (DatePickerDialog.OnDateSetListener)
+                            getActivity(), year, month, day);
+        }
+
     }
 }
