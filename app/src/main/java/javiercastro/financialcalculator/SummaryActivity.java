3e464b7fbb51826a80a536308eb99248e6cd4f12
santoshnpl54 <santoshnpl54@gmail.com>
2018-04-11 20:27:53 -0500

    summary activity

---
+++ app/src/main/java/javiercastro/financialcalculator/SummaryActivity.java
@@ -0,0 +1,14 @@
+package javiercastro.financialcalculator;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SummaryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+    }
+
+}
